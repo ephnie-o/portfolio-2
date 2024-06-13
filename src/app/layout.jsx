7315d74d69f08029a9c97089227355cdaffc69ed
Stephanie Odoom <styzyl18@gmail.com>
2024-06-13 18:56:35 +0000

    minor

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,18 +12,14 @@ export const metadata = {
   },
   description:
     'I&apos;m Stephanie, a software programmer and pharmacist based in Ghana. I&apos;m the lead front end developer and assistant managing pharmacist at Stereda pharmaceuticals limited.',
+  openGraph: {
+    images: '/opengragh-image.jpeg',
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
-      <Head>
-        <meta property='og:title' content="Stephanie&apos;s Advanced Portolio" />
-        <meta property='og:description' content="An improved portfolio as my skills expand." />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://stephanieodoom2.com" />
-        <meta property="og:image" content="https://stephanieodoom.com/Me.jpeg" />
-      </Head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
           <div className="flex w-full">
